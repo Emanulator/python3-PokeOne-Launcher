ffd37083566fa1ab4887cce16212b5d7dc1e6401
Emanulator <emanuel2001just@gmail.com>
2023-02-23 19:02:13 +0100

    Add files via upload

---
+++ install-p1-steamdeck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+flatpak install flathub com.usebottles.bottles
+flatpak run --command=bottles-cli com.usebottles.bottles new --bottle-name P1 --environment gaming
+
+git clone https://github.com/sonic2kk/steamtinkerlaunch /home/deck/steamtinkerlaunch
+chmod +x /home/deck/steamtinkerlaunch/steamtinkerlaunch
+wget 
+
+chmod +x /home/deck/PokeOne.AppImage
+
+/home/deck/steamtinkerlaunch/steamtinkerlaunch ansg -ep=/home/deck/PokeOne.AppImage --appname=PokeOne -lo="LD_PRELOAD=${LD_PRELOAD/_32/_64} QT_SCALE_FACTOR=1.25 %command% --fullscreen --notransparency --new-tab --hide-menubar --qwindowgeometry 1024x640"
+
+read -p "Press any key to continue "
