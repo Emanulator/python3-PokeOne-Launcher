062932f28bc2d074ec292fe394db601b7c29233d
Emanulator <emanuel2001just@gmail.com>
2024-05-04 17:59:25 +0200

    fixed steamdeck install script if run on systems like bazzite

--- install-p1-steamdeck.sh
+++ install-p1-steamdeck.sh
@@ -1,12 +1,13 @@
 flatpak --user --assumeyes --noninteractive install flathub com.usebottles.bottles
 flatpak run --command=bottles-cli com.usebottles.bottles new --bottle-name P1 --environment gaming
 
-git clone https://github.com/sonic2kk/steamtinkerlaunch /home/deck/steamtinkerlaunch
-chmod +x /home/deck/steamtinkerlaunch/steamtinkerlaunch
-wget https://github.com/emanuel2001just/python3-PokeOne-Launcher/releases/download/steamdeck-test1/PokeOne.Launcher.SteamDeck.AppImage -O /home/deck/PokeOne.AppImage
+git clone https://github.com/sonic2kk/steamtinkerlaunch ~/steamtinkerlaunch
+chmod +x ~/steamtinkerlaunch/steamtinkerlaunch
+wget https://github.com/emanuel2001just/python3-PokeOne-Launcher/releases/download/steamdeck-test1/PokeOne.Launcher.SteamDeck.AppImage -O ~/PokeOne.AppImage
 
-chmod +x /home/deck/PokeOne.AppImage
+chmod +x ~/PokeOne.AppImage
+unset HOME; export HOME=~
 
-/home/deck/steamtinkerlaunch/steamtinkerlaunch ansg -ep=/home/deck/PokeOne.AppImage --appname=PokeOne -lo="LD_PRELOAD=${LD_PRELOAD/_32/_64} QT_SCALE_FACTOR=1.25 %command% --fullscreen --notransparency --new-tab --hide-menubar --qwindowgeometry 1024x640"
+~/steamtinkerlaunch/steamtinkerlaunch ansg -ep=$HOME/PokeOne.AppImage --appname="PokeOne" -lo="LD_PRELOAD=${LD_PRELOAD/_32/_64} QT_SCALE_FACTOR=1.25 %command% --fullscreen --notransparency --new-tab --hide-menubar --qwindowgeometry 1024x640"
 
 read -p "Press any key to continue "
