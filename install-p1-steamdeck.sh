20d5ba9edc73a0e0bdfc199b49420e62162f96ce
Emanulator <emanuel2001just@gmail.com>
2024-05-07 21:36:21 +0200

    Update install-p1-steamdeck.sh

--- install-p1-steamdeck.sh
+++ install-p1-steamdeck.sh
@@ -1,11 +1,19 @@
 #flatpak --user --assumeyes --noninteractive install flathub com.usebottles.bottles
+echo "Please choose 'user' if asked!"
 flatpak --assumeyes install flathub com.usebottles.bottles
+export FORCE_OFFLINE=0
+echo ""
+echo ""
+echo "Please Complete the Bottles Setup and close it afterwards"
+echo ""
+read -p "Press any key to continue when ready "
+
 
 flatpak run --command=bottles-cli com.usebottles.bottles new --bottle-name P1 --environment gaming
-
+flatpak override --user com.usebottles.bottles --filesystem="~/PokeOne"
 git clone https://github.com/sonic2kk/steamtinkerlaunch ~/steamtinkerlaunch
 chmod +x ~/steamtinkerlaunch/steamtinkerlaunch
-wget https://github.com/emanuel2001just/python3-PokeOne-Launcher/releases/download/steamdeck-test1/PokeOne.Launcher.SteamDeck.AppImage -O ~/PokeOne.AppImage
+#wget https://github.com/emanuel2001just/python3-PokeOne-Launcher/releases/download/steamdeck-test1/PokeOne.Launcher.SteamDeck.AppImage -O ~/PokeOne.AppImage
 
 chmod +x ~/PokeOne.AppImage
 unset HOME; export HOME=~
